cd0f46b81773c3362f7bd19a0233cf92c42e4467
Tahseen yousuf <tehseenyousuf@gmail.com>
2023-12-23 18:56:22 +0530

    ME_QA_XGOOGLE_CALC1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -2,6 +2,9 @@ package demo;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -49,7 +52,97 @@ public class TestCases {
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        if(driver.getCurrentUrl().contains("google")){
+            WebElement googleTextArea =  driver.findElement(By.id("APjFqb"));
+            googleTextArea.sendKeys("calculator");
+            googleTextArea.sendKeys(Keys.ENTER);
+            driver.findElement(By.xpath("//span[@jsname='VssY5c']")).getText().contains("0");
+            System.out.println("testCase01 PASSED");
+        }
+        else{
+            System.out.println("something isn't right");
+        }
+
+    }
+
+    public void testcase02(){
+
+        System.out.println("Start Test case: testCase02");
+        try {
+
+            driver.findElement(By.xpath("//div[text()='5']")).click();
+            driver.findElement(By.xpath("//div[text()='+']")).click();
+            driver.findElement(By.xpath("//div[text()='7']")).click();
+            driver.findElement(By.xpath("//div[text()='=']")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//span[@jsname='VssY5c']")).getText().equals("12");
+            driver.findElement(By.xpath("//div[text()='AC']")).click();
+            Thread.sleep(1000);
+            driver.findElement(By.xpath("//div[text()='1']")).click();
+            driver.findElement(By.xpath("//div[text()='5']")).click();
+            driver.findElement(By.xpath("//div[@jsname='pPHzQc']")).click();
+            driver.findElement(By.xpath("//div[text()='8']")).click();
+            driver.findElement(By.xpath("//div[text()='=']")).click();
+            driver.findElement(By.xpath("//span[@jsname='VssY5c']")).getText().equals("7");
+            driver.findElement(By.xpath("//div[text()='AC']")).click();
+            Thread.sleep(2000);
+            System.out.println("testCase02 PASSED");
+
+        }
+        catch(Exception e) {
+            System.out.println("something isn't right");
+        }
+
+    }
+
+    public void testcase03(){
+
+        System.out.println("Start Test case: testCase03");
+        try {
+
+            driver.findElement(By.xpath("//div[text()='1']")).click();
+            driver.findElement(By.xpath("//div[text()='0']")).click();
+            driver.findElement(By.xpath("//div[@jsname='YovRWb']")).click();
+            driver.findElement(By.xpath("//div[text()='3']")).click();
+            driver.findElement(By.xpath("//div[text()='=']")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//span[@jsname='VssY5c']")).getText().equals("30");
+            driver.findElement(By.xpath("//div[text()='AC']")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//span[@jsname='VssY5c']")).getText().equals("0");
+            Thread.sleep(2000);
+
+            System.out.println("testCase03 PASSED");
+
+        }
+        catch(Exception e) {
+            System.out.println("something isn't right");
+        }
+
+    }
+
+    public void testcase04(){
+
+        System.out.println("Start Test case: testCase04");
+        try {
+
+            driver.findElement(By.xpath("//div[text()='2']")).click();
+            driver.findElement(By.xpath("//div[text()='0']")).click();
+            driver.findElement(By.xpath("//div[@jsname='WxTTNd']")).click();
+            driver.findElement(By.xpath("//div[text()='4']")).click();
+            driver.findElement(By.xpath("//div[text()='=']")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//span[@jsname='VssY5c']")).getText().equals("5");
+            driver.findElement(By.xpath("//div[text()='AC']")).click();
+            Thread.sleep(2000);
+
+            System.out.println("testCase04 PASSED");
+
+        }
+        catch(Exception e) {
+            System.out.println("something isn't right");
+        }
+
     }
 
 
